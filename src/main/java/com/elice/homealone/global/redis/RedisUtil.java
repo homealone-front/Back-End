dce70471925a8e851ee68066dd26732ac9f41633
Lazy Cat <mylazysundaycat@gmail.com>
2024-12-28 23:02:41 +0900

    refactor: RedisUtil 코드 정리

--- src/main/java/com/elice/homealone/global/redis/RedisUtil.java
+++ src/main/java/com/elice/homealone/global/redis/RedisUtil.java
@@ -14,19 +14,43 @@
     @Component
     @RequiredArgsConstructor
     public class RedisUtil {
-        private final RedisTemplate<String, Object> redisBlackListTemplate;
+        private final RedisTemplate<String, Object> redisTemplate;
         @Value("${spring.jwt.token.access-expiration-time}")
         private long expirationTime;
 
+
+        /**
+         * 시간은 MILLISECONDS 단위로 저장
+         */
+        public void set(String key, Object value, long milliseconds) {
+            redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(value.getClass()));
+            redisTemplate.opsForValue().set(key, value, milliseconds, TimeUnit.MILLISECONDS);
+        }
+
+        public Object get(String key) {
+            return redisTemplate.opsForValue().get(key);
+        }
+
+        public boolean delete(String key) {
+            return redisTemplate.delete(key);
+        }
+
+        public boolean hasKey(String key) {
+            return redisTemplate.hasKey(key);
+        }
+
+        /**
+         * jwt blackList 관리용
+         */
         public void setBlackList(String key, Object o) {
-            redisBlackListTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(o.getClass()));
-            redisBlackListTemplate.opsForValue().set(key, o, expirationTime, TimeUnit.MILLISECONDS);
+            redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(o.getClass()));
+            redisTemplate.opsForValue().set(key, o, expirationTime, TimeUnit.MILLISECONDS);
         }
 
         public boolean hasKeyBlackList(String key) {
-            if (redisBlackListTemplate == null) {
+            if (redisTemplate == null) {
                 throw new HomealoneException(ErrorCode.REDIS_NOT_INITIALIZED);
             }
-            return Boolean.TRUE.equals(redisBlackListTemplate.hasKey(key));
+            return Boolean.TRUE.equals(redisTemplate.hasKey(key));
         }
     }
--- src/main/java/com/elice/homealone/module/login/controller/OAuthController.java
+++ src/main/java/com/elice/homealone/module/login/controller/OAuthController.java
@@ -35,25 +35,6 @@ public class OAuthController {
         System.out.println("이거까지 찍히면 진짜 토큰 발급 되는거임");
         return new ResponseEntity<>(tokenDto, HttpStatus.OK);
     }
-//
-//    //로컬 테스트 코드
-//    private final OAuthService oAuthService;
-//    @Operation(summary = "소셜 로그인 페이지 리다이렉트")
-//    @GetMapping("/{platform}")
-//    public ResponseEntity<Void> OAuth2LoginRedirect(@PathVariable String platform) {
-//        String redirectUrl = oAuthService.getRedirectUri(platform);
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setLocation(URI.create(redirectUrl));
-//        return new ResponseEntity<>(headers, HttpStatus.FOUND);
-//    }
-//    @Operation(summary = "소셜 로그인 콜백 (code 수신 및 accessToken 발급)")
-//    @GetMapping ("/{platform}/callback")
-//    public String getAccessToken (@PathVariable String platform, @RequestParam String code, HttpServletResponse response){
-//        System.out.println("요청은 받는지 테스트");
-//        TokenDto tokenDto = oAuthService.processOAuthLogin(platform, code, response);
-//        System.out.println("이거까지 찍히면 진짜 토큰 발급 되는거임");
-//        return tokenDto.getAccessToken();
-//    }
 
 }
 
