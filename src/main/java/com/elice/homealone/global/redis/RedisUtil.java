aeda5bcdc25db2280f54cb09ab6d270af48383bb
Lazy Cat <mylazysundaycat@gmail.com>
2024-12-28 22:59:28 +0900

    refactor: RedisUtil 사용하지 않는 코드 삭제

--- src/main/java/com/elice/homealone/global/redis/RedisUtil.java
+++ src/main/java/com/elice/homealone/global/redis/RedisUtil.java
@@ -14,46 +14,13 @@
     @Component
     @RequiredArgsConstructor
     public class RedisUtil {
-        private final RedisTemplate<String, Object> redisTemplate;
         private final RedisTemplate<String, Object> redisBlackListTemplate;
         @Value("${spring.jwt.token.access-expiration-time}")
         private long expirationTime;
 
-
-        /**
-         * 시간은 MILLISECONDS 단위로 저장
-         */
-        public void set(String key, Object value, long milliseconds) {
-            redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(value.getClass()));
-            redisTemplate.opsForValue().set(key, value, milliseconds, TimeUnit.MILLISECONDS);
-        }
-
-        public Object get(String key) {
-            return redisTemplate.opsForValue().get(key);
-        }
-
-        public boolean delete(String key) {
-            return redisTemplate.delete(key);
-        }
-
-        public boolean hasKey(String key) {
-            return redisTemplate.hasKey(key);
-        }
-
-        /**
-         * jwt blackList 관리용
-         */
         public void setBlackList(String key, Object o) {
             redisBlackListTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(o.getClass()));
             redisBlackListTemplate.opsForValue().set(key, o, expirationTime, TimeUnit.MILLISECONDS);
-        }
-
-        public Object getBlackList(String key) {
-            return redisBlackListTemplate.opsForValue().get(key);
-        }
-
-        public boolean deleteBlackList(String key) {
-            return redisBlackListTemplate.delete(key);
         }
 
         public boolean hasKeyBlackList(String key) {
